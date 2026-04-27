6c3f8ea4c9cfb65266ef335e8ccfef56ef59eef0
Mohanatheeba <mohanamanikandan17@gmail.com>
2026-04-27 17:26:01 +0530

    Build: Hard Reset to Qwen Stable v1.1.2

--- brain/main.py
+++ brain/main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-# VERSION: 1.0.9 - TRIPLE FORCE RESET
+# VERSION: 1.1.2 - QWEN STABLE
 
 load_dotenv()
 
